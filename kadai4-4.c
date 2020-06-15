46116f15323266f7bbe452e7feb5b2959a3c48e1
T180039 <49540809+T180039@users.noreply.github.com>
2020-06-15 13:33:18 +0900

    Update kadai4-4.c

--- kadai4-4.c
+++ kadai4-4.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 
-int main() {
-    int i = 1;
-    printf("%d\n", i)
+int main(){
+    int i=1;
+
+    int j=1,k=0;
     while(1){
-        i++;
-        if(i<0)
-            break;
+        i*=2;
+        if(i<0){
+            i=j;
+            while(1){
+                j/=2;
+                i+=j;
+                if(i==k) break;
+                k=i;
+            }
+        }
+        j=i;
+        if(i==k) break;
     }
-    i--;
     printf("%d\n",i);
 }
